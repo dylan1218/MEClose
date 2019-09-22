74b22e0d7b4b5e572a4fac50d19eed3a496400fc
dylan1218@live.com <dylan1218@live.com>
2019-09-22 13:59:08 -0400

    Django Channels

--- MonthEndCloseDjango/settings.py
+++ MonthEndCloseDjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'channels',
     'crispy_forms',
     'CloseApplication.apps.MonthEndCloseConfig',
 ]
@@ -76,6 +77,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'MonthEndCloseDjango.wsgi.application'
+
+#For channels implementation
+ASGI_APPLICATION = "CloseApplication.routing.application"
 
 
 # Database
